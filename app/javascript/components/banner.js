576fa77b306863f4cc4a6323f881eab88bc3cd8f
Chiwei Huang <cwhuang0523@gmail.com>
2019-01-03 14:33:16 +0800

    add favicon and fix show page message

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
     new Typed('#banner-typed-text', {
-        strings: ["Enter a job keyword.", "You can get salary, location, skills analysis."],
+        strings: ["Want to know job market in China?","Enter a job keyword.", "You can get salary, location, skills analysis."],
         typeSpeed: 40,
         loop: true
     });
